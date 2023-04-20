635eff65a0251f1fa94b2e59eaf1ae089f3d4d92
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-04-20 07:54:59 +0300

    pascal_triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""Module implements pascal triangle"""
+
+
+def pascal_triangle(n):
+    """Function implements pascal traingle"""
+    current = []
+    result = []
+    if n <= 0:
+        return []
+    for i in range(n + 1):
+        if i == 1:
+            result.append([1])
+        if i == 2:
+            result.append([1, 1])
+            current.extend([1, 1])
+        if i >= 3:
+            new = []
+            for j in range(len(current)-1):
+                new.append(current[j] + current[j+1])
+            new.insert(0, 1)
+            new.append(1)
+            result.append(new)
+            current.clear()
+            current.extend(new)
+    return result
+
+
+print(pascal_triangle(6))
