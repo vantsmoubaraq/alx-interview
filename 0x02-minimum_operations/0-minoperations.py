007c578a8dc4bbc856f9b5d7fdb4c412a7e0e537
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-03 09:04:32 +0300

    min_operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-""" text file containing a single character H which can only execute
-copy all and paste given a number n"""
+
+"""Module calculates the fewest number of operations"""
 
 
 def minOperations(n):
-    """creates a list of operations with 0 as first element and
-    inf->infinity which initialize operations"""
-    operations = [0] + [float('inf')] * n
-    for i in range(2, n+1):
-        for j in range(1, i):
-            """check if j is a divisor of i"""
-            if i % j == 0:
-                """update the value of operations[i] to be the min, then find
-                out how many times needed to paste j H's to get i H's"""
-                operations[i] = min(operations[i], operations[j] + i // j)
-    """return an int, if n is impossible to achieve return 0"""
-    return operations[n] if operations[n] < float('inf') else 0
+    """calculates the fewest number of operations"""
+    copy_ops = 0
+    paste_ops = 0
+
+    if n <= 0 >= float("inf") or type(n) is not int:
+        return 0
+
+    while (n > 1):
+        max_num = 0
+        mod_list = []
+        for i in range(1, n):
+            if n % i == 0:
+                mod_list.append(i)
+
+        max_num = max(mod_list)
+        copy_ops += 1
+        paste_ops += ((n // max_num) - 1)
+
+        n = max_num
+
+    total = copy_ops + paste_ops
+    return total
