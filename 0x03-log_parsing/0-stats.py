ec9cebc5a142cc4607c705bdee0e244c5229b49d
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-09 21:22:03 +0300

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,7 +3,7 @@
 """reads stdin line by line and computes metrics"""
 
 import sys
-
+import re
 
 
 def main():
@@ -14,13 +14,38 @@ def main():
 
     file_size = 0
     count = 0
+   
     while True:
-            line = sys.stdin.readline()
-            if not line:
-                break
+        line = sys.stdin.readline()
+        if not line:
+            break
 
-            st = r'\[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
-            pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - ' + st
+        st = r'\[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
+        pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - ' + st
+
+        match = re.match(pattern, line)
+        if not match:
+            continue
+
+        status_code = str(match.group(3))
+
+        if status_code in current_dict:
+            count += 1
+            current_dict[status_code] += 1
+            file_size += int(match.group(4))
+
+        try:
+            if count == 10:
+                print(f"File size: {file_size}")
+                for key, value in sorted(current_dict.items(),
+                                        key=lambda x: x):
+                    print(f"{key}: {value}")
+                count = 0
+        except KeyboardInterrupt as e:
+            print(f"File size: {file_size}")
+            for key, value in sorted(current_dict.items(), key=lambda x: x):
+                print(f"{key}: {value}")
+            print(e)
 
 
 if __name__ == "__main__":
