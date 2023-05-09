52734f6756952d8d45eeaab57da0be5f4ad06f22
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-09 21:15:04 +0300

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -23,23 +23,7 @@ def main():
             st = r'\[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
             pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - ' + st
 
-            match = re.match(pattern, line)
-            if not match:
-                continue
-
-            status_code = str(match.group(3))
-
-            if status_code in current_dict:
-                count += 1
-                current_dict[status_code] += 1
-                file_size += int(match.group(4))
-
-            if count == 10:
-                print(f"File size: {file_size}")
-                for key, value in sorted(current_dict.items(),
-                                         key=lambda x: x):
-                    print(f"{key}: {value}")
-                count = 0
+            
     except KeyboardInterrupt as e:
         print(f"File size: {file_size}")
         for key, value in sorted(current_dict.items(), key=lambda x: x):
