38605919d613a5af8c5608b4f1a7f8985079c5b7
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-09 21:23:34 +0300

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -34,18 +34,14 @@ def main():
             current_dict[status_code] += 1
             file_size += int(match.group(4))
 
-        try:
-            if count == 10:
-                print(f"File size: {file_size}")
-                for key, value in sorted(current_dict.items(),
-                                        key=lambda x: x):
-                    print(f"{key}: {value}")
-                count = 0
-        except KeyboardInterrupt as e:
+        
+        if count == 10:
             print(f"File size: {file_size}")
-            for key, value in sorted(current_dict.items(), key=lambda x: x):
+            for key, value in sorted(current_dict.items(),
+                                     key=lambda x: x):
                 print(f"{key}: {value}")
-            print(e)
+            count = 0
+        
 
 
 if __name__ == "__main__":
