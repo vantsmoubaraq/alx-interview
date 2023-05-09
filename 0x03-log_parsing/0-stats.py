ff5e332ed0e8193be8a28f20c5c4bf80c5087f9a
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-09 17:21:43 +0300

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,51 @@
 #!/usr/bin/python3
 
+"""reads stdin line by line and computes metrics"""
+
 import sys
 import re
 
-  
-def printsts(dic, size):
-    """ WWPrints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+
+def main():
+    """reads stdin line by line and computes metrics"""
+    current_dict = {}
+    for num in [200, 301, 400, 401, 403, 404, 405, 500]:
+        current_dict[str(num)] = 0
+
+    file_size = 0
+    count = 0
+    try:
+        while True:
+            line = sys.stdin.readline()
+            if not line:
+                break
+
+            st = r'\[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
+            pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - ' + st
+
+            match = re.match(pattern, line)
+            if not match:
+                continue
+
+            status_code = str(match.group(3))
+
+            if status_code in current_dict:
+                count += 1
+                current_dict[status_code] += 1
+                file_size += int(match.group(4))
+
+            if count == 10:
+                print(f"File size: {file_size}")
+                for key, value in sorted(current_dict.items(),
+                                         key=lambda x: x):
+                    print(f"{key}: {value}")
+                count = 0
+    except KeyboardInterrupt as e:
+        print(f"File size: {file_size}")
+        for key, value in sorted(current_dict.items(), key=lambda x: x):
+            print(f"{key}: {value}")
+        print(e)
 
 
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
-
-try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
-
-        stlist = line.split()
-        count += 1
-
-        try:
-            size += int(stlist[-1])
-        except Exception:
-            pass
-
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except Exception:
-            pass
-    printsts(sts, size)
-except KeyboardInterrupt as e:
-    printsts(sts, size)
-    raise
-
-
+if __name__ == "__main__":
+    main()
