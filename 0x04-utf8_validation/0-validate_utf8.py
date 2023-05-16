c5fc187c624e8b59568286161b098cb4fc54abd6
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-16 07:12:07 +0300

    validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,8 +5,8 @@
 
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding"""
-    minimum = 2 ** (-7)
-    maximum = (2 ** 7) - 1
+    minimum = 2 ** (-8)
+    maximum = (2 ** 8) - 1
 
     for item in data:
         if item < minimum or item > maximum:
