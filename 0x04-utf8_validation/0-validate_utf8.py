4f45bd69737f239d92f26d8138133749d23e50a5
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-17 16:01:17 +0300

    validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,8 +5,22 @@
 
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding"""
-    for item in data:
-        left_most_bit = (item >> 7) & 1
-        if left_most_bit == 1:
-            return False
-    return True
+    """validate data if it is utf-8"""
+    state = 0
+    for num in data:
+        bit = 0b10000000
+        if not state:
+            while (bit & num):
+                state += 1
+                bit >>= 1
+            if state > 4:
+                return False
+            if state:
+                state -= 1
+                if state == 0:
+                    return False
+        elif state > 0:
+            if num >> 6 != 2:
+                return False
+            state -= 1
+    return not state
